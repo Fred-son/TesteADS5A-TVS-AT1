50a2e8466e3599cc30900f6e74537e4b681606a7
Fredson Oliveira de Jesus <r5508720@tesblan.local>
2024-04-17 17:33:22 -0300

    Alteração de dados

---
+++ recebeDados.test.js
@@ -0,0 +1,12 @@
+
+const dadosEstudante = require('./dadosEstudante');
+
+describe('Teste da função recebeDados', () => {
+  test('Verifica se a função processa corretamente o nome "Fredson Oliveira" e o RA "193074"', () => {
+    const nome = "Fredson Oliveira";
+    const RA = "193074";
+    const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
+
+    expect(dadosEstudante(nome, RA)).toBe(esperado);
+  });
+});
